a92d4be93de8a8ab8ba107e02a261099c913afd0
Nailu <contact@nailu.fr>
2023-03-14 13:39:44 +0100

    Randomized map generation, obstacle generation is based on rate and not a value anymore (ex: 25%)

--- main.js
+++ main.js
@@ -1,41 +1,57 @@
+// Pets and obstacles list
 const obstacleList = ['🪨', '🌋', '🛖', '🏠', '🌳', '🌴'];
 const pathIcoList = ['🐕', '🐈', '🐅', '🐎', '🦌', '🐄', '🐪', '🦣', '🐿️', '🦫', '🦔', '🦇', '🦘', '🦃', '🦆', '🦆', '🐊', '🐢', '🦖', '🦈', '🐋', '🐛', '🐞', '🕷️', '🐌'];
 
+// Get random pet and obstacles icon
 const obstacle = obstacleList[aleatoire(0, taille(obstacleList) - 1)]
 const pathIco = pathIcoList[aleatoire(0, taille(pathIcoList) - 1)]
 
+// Default icons
 const grass = "🌿";
 const errorIco = "❌";
 const pawPrint = "🐾";
 
-const startPos = [aleatoire(0, 9), aleatoire(0, 9)];
-const endPos = [aleatoire(0, 9), aleatoire(0, 9)];
+// Random map size
+const mapSize = aleatoire(10, 20);
 
-const map = [
-    [grass, grass, grass, grass, grass, grass, grass, grass, grass, grass],
-    [grass, grass, grass, grass, grass, grass, grass, grass, grass, grass],
-    [grass, grass, grass, grass, grass, grass, grass, grass, grass, grass],
-    [grass, grass, grass, grass, grass, grass, grass, grass, grass, grass],
-    [grass, grass, grass, grass, grass, grass, grass, grass, grass, grass],
-    [grass, grass, grass, grass, grass, grass, grass, grass, grass, grass],
-    [grass, grass, grass, grass, grass, grass, grass, grass, grass, grass],
-    [grass, grass, grass, grass, grass, grass, grass, grass, grass, grass],
-    [grass, grass, grass, grass, grass, grass, grass, grass, grass, grass],
-    [grass, grass, grass, grass, grass, grass, grass, grass, grass, grass]
-]
+// Random starting and ending coords
+const startPos = [aleatoire(0, mapSize), aleatoire(0, mapSize)];
+const endPos = [aleatoire(0, mapSize), aleatoire(0, mapSize)];
 
-const generateRandomObstacle = number => {
-    for(let i = 0; i < number; i++) {
-        map[aleatoire(0, 9)][aleatoire(0, 9)] = obstacle;
+// Generate map (array) depending on size
+const getMap = size => {
+    tmp = [];
+
+    for(let i = 0; i < size; i++) {
+        push(tmp, [])
+        for(let j = 0; j < size; j++) {
+            push(tmp[i], grass)
+        }
+    }
+
+    return tmp;
+}
+
+// Get map
+const map = getMap(mapSize);
+
+// Generate the coords of obstacle
+const generateRandomObstacle = rate => {
+
+    // Get the number of obstacles based on a pourcentage of all tiles
+    size = mapSize * mapSize;
+    pourcentage = (size * rate) / 100;
+
+    // Set obstacles in the array
+    for(let i = 0; i < pourcentage; i++) {
+        map[aleatoire(0, mapSize - 1)][aleatoire(0, mapSize - 1)] = obstacle;
     }
 }
 
-const isObstacle = coords => {
-    console.log(coords[0], coords[1]);
-    console.log(map[coords[0]][coords[1]] === obstacle)
-    return map[coords[0]][coords[1]] === obstacle;
-}
+// Verify if a tile contain an obstacle
+const isObstacle = coords => map[coords[0]][coords[1]] === obstacle;
 
+// Get path, array ouput with coords (ex: [[1, 3], [2, 3], [3, 3]])
 const getPath = (startPos, endPos) => {
     path = [];
 
@@ -88,6 +104,7 @@ const getPath = (startPos, endPos) => {
     return path;
 }
 
+// Set path ico depending on coords in game grid
 const displayPath = path => {
     for(let i = 0; i < taille(path); i++) {
         map[path[i][0]][path[i][1]] = pawPrint;
@@ -95,9 +112,11 @@ const displayPath = path => {
     map[path[taille(path) - 1][0]][path[taille(path) - 1][1]] = pathIco;
 }
 
-afficher(`Start : <b>${startPos[0] + 1}:${startPos[1] + 1}</b> - End : <b>${endPos[0] + 1}:${endPos[1] + 1}</b>`)
 
-generateRandomObstacle(20);
+// INIT
+afficher(`Start : <b>${startPos[0] + 1}:${startPos[1] + 1}</b> - End : <b>${endPos[0] + 1}:${endPos[1] + 1}</b> - Map Size : <b>${mapSize}</b>`)
+
+generateRandomObstacle(50);
 
 displayPath(getPath(startPos, endPos));
 
