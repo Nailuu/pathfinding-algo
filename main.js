d6509745c31a6af5d1bb844344b74c314885e5b7
Nailu <contact@nailu.fr>
2023-03-14 11:24:08 +0100

    obstacle detection system

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const obstacle = "🪨";
 const grass = "🌿";
 const pathIco = "🐌";
+const errorIco = "🟥";
 
-const startPos = [aleatoire(1, 10), aleatoire(1, 10)];
-const endPos = [aleatoire(1, 10), aleatoire(1, 10)];
+const startPos = [aleatoire(0, 9), aleatoire(0, 9)];
+const endPos = [aleatoire(0, 9), aleatoire(0, 9)];
 
 const map = [
     [grass, grass, grass, grass, grass, grass, grass, grass, grass, grass],
@@ -24,28 +25,58 @@ const generateRandomObstacle = number => {
     }
 }
 
+const isObstacle = coords => {
+    console.log(coords[0], coords[1]);
+    console.log(map[coords[0]][coords[1]] === obstacle)
+    return map[coords[0]][coords[1]] === obstacle;
+}
+
 const getPath = (startPos, endPos) => {
     path = [];
 
     // Y AXIS MOVEMENT
     if(startPos[0] > endPos[0]) {
         for(let i = startPos[0]; i >= endPos[0]; i--) {
-            push(path, [i, startPos[1]])
+
+            // if there is an obstacle then find another way
+            if(isObstacle([i, startPos[1]])) {
+                map[i][startPos[1]] = errorIco;
+            } else {
+                push(path, [i, startPos[1]])
+            }
         }
     } else {
         for(let i = startPos[0]; i <= endPos[0]; i++) {
-            push(path, [i, startPos[1]])
+
+            // if there is an obstacle then find another way
+            if(isObstacle([i, startPos[1]])) {
+                map[i][startPos[1]] = errorIco;
+            } else {
+                push(path, [i, startPos[1]])
+            }
         }
     }
 
     // X AXIS MOVEMENT
     if(startPos[1] > endPos[1]) {
-        for(let i = startPos[1] - 1; i >= endPos[1]; i--) {
-            push(path, [endPos[0], i])
+        for(let i = startPos[1]; i >= endPos[1]; i--) {
+
+            // if there is an obstacle then find another way
+            if(isObstacle([endPos[0], i])) {
+                map[endPos[0]][i] = errorIco;
+            } else {
+                push(path, [endPos[0], i])
+            }
         }
     } else {
-        for(let i = startPos[1] + 1; i <= endPos[1]; i++) {
-            push(path, [endPos[0], i])
+        for(let i = startPos[1]; i <= endPos[1]; i++) {
+
+            // if there is an obstacle then find another way
+            if(isObstacle([endPos[0], i])) {
+                map[endPos[0]][i] = errorIco;
+            } else {
+                push(path, [endPos[0], i])
+            }
         }
     }
 
@@ -54,14 +85,15 @@ const getPath = (startPos, endPos) => {
 
 const displayPath = path => {
     for(let i = 0; i < taille(path); i++) {
-        map[path[i][0] - 1][path[i][1] - 1] = pathIco;
-        console.log(path[i][0], path[i][1])
+        map[path[i][0]][path[i][1]] = pathIco;
     }
 }
 
-afficher(`Start : <b>${startPos}</b> - End : <b>${endPos}</b>`)
+afficher(`Start : <b>${startPos[0] + 1}:${startPos[1] + 1}</b> - End : <b>${endPos[0] + 1}:${endPos[1] + 1}</b>`)
 
-generateRandomObstacle(15);
+generateRandomObstacle(40);
+
+afficher(map);
 
 displayPath(getPath(startPos, endPos));
 
